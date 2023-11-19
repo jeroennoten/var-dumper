546f248b18f587f75e57bbf3ed5a57ebba40b099
Jeroen Noten <jeroennoten@me.com>
2023-11-19 19:23:14 +0100

    special chars

--- Dumper/HtmlDumper.php
+++ Dumper/HtmlDumper.php
@@ -878,7 +878,7 @@ EOHTML
         }
         $this->lastDepth = $depth;
 
-        $this->line = mb_convert_encoding($this->line, 'HTML-ENTITIES', 'UTF-8');
+        $this->line = mb_encode_numericentity($this->line, [0x80, 0x10FFFF, 0, ~0], 'UTF-8');
 
         if (-1 === $depth) {
             AbstractDumper::dumpLine(0);
